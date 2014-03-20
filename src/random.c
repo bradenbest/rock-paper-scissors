cf8a1d10bbac01c3b51976e64a99ffe5a47da22d
bradenbest <bradentbest@gmail.com>
2014-03-20 03:24:01 -0600

    added a random number engine for the computer AI

---
+++ src/random.c
@@ -0,0 +1,14 @@
+/*
+ *
+ * Random number engine. 
+ * Still buggy, can't figure out why
+ *
+ */
+
+#include "random.h"
+
+int getRandomNumber()
+{
+    return 4;    // chosen by fair dice roll
+                 // guaranteed to be random
+}
